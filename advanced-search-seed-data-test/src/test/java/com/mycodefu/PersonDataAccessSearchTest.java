bb65c6e5afc22367ef61ebf86a6416573155c38a
luketn <endeavour9@gmail.com>
2025-03-30 12:49:25 +1100

    use a mongodump bson gzip instead of mongoexport to simplify data loading

--- advanced-search-seed-data-test/src/test/java/com/mycodefu/PersonDataAccessSearchTest.java
+++ advanced-search-seed-data-test/src/test/java/com/mycodefu/PersonDataAccessSearchTest.java
@@ -1,17 +1,26 @@
 package com.mycodefu;
 
+import com.mongodb.client.ListSearchIndexesIterable;
 import com.mycodefu.PersonDataAccess.Person;
+import org.bson.BsonDocument;
+import org.bson.Document;
 import org.junit.jupiter.api.*;
 import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.ExecConfig;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.mongodb.MongoDBAtlasLocalContainer;
+import org.testcontainers.shaded.com.google.common.io.Resources;
+import org.testcontainers.shaded.org.awaitility.Awaitility;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.testcontainers.shaded.org.apache.commons.lang3.ArrayUtils.toArray;
 
@@ -30,15 +39,27 @@ class PersonDataAccessSearchTest {
 
     @BeforeAll
     static void beforeAll() throws IOException, InterruptedException {
-        Instant start = Instant.now();
-        mongoDBContainer.execInContainer(ExecConfig.builder()
-                .workDir("/tmp/seed-data")
-                .command(toArray("bash", "seed-data.sh"))
-                .build());
-        System.out.println("Loading seed data took: " + Instant.now().minusMillis(start.toEpochMilli()).toEpochMilli() + "ms");
-
         System.out.println("Initializing data access with MongoDB connection string: " + mongoDBContainer.getConnectionString());
         personDataAccess = new PersonDataAccess(mongoDBContainer.getConnectionString());
+
+        Instant startSeedDataRestore = Instant.now();
+        mongoDBContainer.execInContainer(ExecConfig.builder()
+                .workDir("/tmp/seed-data")
+                .command(toArray("mongorestore", "--gzip"))
+                .build());
+        System.out.println("Loading seed data took: " + Instant.now().minusMillis(startSeedDataRestore.toEpochMilli()).toEpochMilli() + "ms");
+
+        Instant startIndex = Instant.now();
+        String personSearchMappings = Resources.toString(Resources.getResource("seed-data/dump/examples/person_search.json"), UTF_8);
+        personDataAccess.collection.createSearchIndex("person_search", BsonDocument.parse(personSearchMappings));
+        Awaitility.await()
+                .atMost(10, TimeUnit.SECONDS)
+                .until(() -> {
+                    ListSearchIndexesIterable<Document> searchIndexes = personDataAccess.collection.listSearchIndexes();
+                    Document personIndex = searchIndexes.into(new ArrayList<>()).stream().filter(index -> index.getString("name").equals("person_search")).findFirst().orElseThrow();
+                    return personIndex.getString("status").equals("READY");
+                });
+        System.out.printf("Index created and ready in %dms%n", Duration.between(startIndex, Instant.now()).toMillis());
     }
 
     @Test
